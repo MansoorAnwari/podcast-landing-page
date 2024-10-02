45d2fd5a3c1ce7fdfb184115999904a7fd8f6d74
Mansoor Anwari <mansooranwary50@gmail.com>
2024-10-02 13:55:49 +0330

    Update Tailwind CSS configuration and add images directory

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ["./*.{html,js}", "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "cta-img": "url('../images/mic.png')",
+      },
+      colors: {
+        primary: "#ED7F39",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
